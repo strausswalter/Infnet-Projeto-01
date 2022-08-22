9c880ad4de7ed705cb11e6010b7a79330898a3a4
strausswalter <strausswalter@gmail.com>
2022-08-22 11:20:14 -0300

    responsividade ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 // Monitorar evento scroll da tela + ao chegar em 50px acrecenta classe .changeNavbar ao elemento nav:
     window.addEventListener("scroll", function(){
-        console.log("Ativado o scroll da tela!!!!!")
+        // console.log("Ativado o scroll da tela!!!!!")
 
       if (document.documentElement.scrollTop > 50) {
         document.querySelector('nav').classList.add('changeNavbar');
